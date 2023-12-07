90339d4054213a5e3eb94b93438959d1fc7fef22
Phi <anhphi099@gmail.com>
2023-12-07 10:29:58 +0100

    setup for part6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import org.example.Solution.DayXSolver;
 import org.example.Solution.day02.Day02Solver;
 import org.example.Solution.day04.Day04Solver;
 import org.example.Solution.day05.Day05Solver;
+import org.example.Solution.day06.Day06Solver;
 
 import java.util.List;
 
@@ -12,7 +13,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<DayXSolver> solvers = List.of(new Day02Solver(), new Day04Solver(), new Day05Solver());
+        List<DayXSolver> solvers = List.of(new Day02Solver(), new Day04Solver(), new Day05Solver(), new Day06Solver());
         for (var solver : solvers){
             solver.printSolutions();
         }
---
+++ src/main/java/org/example/Solution/day06/Day06Solver.java
@@ -0,0 +1,15 @@
+package org.example.Solution.day06;
+
+import org.example.Solution.DayXSolver;
+
+public class Day06Solver implements DayXSolver {
+    @Override
+    public Long partOneSolution() {
+        return null;
+    }
+
+    @Override
+    public Long partTwoSolution() {
+        return null;
+    }
+}
