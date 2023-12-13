eff7af68e9d66c846df9f7f5b8bdeb7e23fed79f
Phi <anhphi099@gmail.com>
2023-12-13 20:01:18 +0100

    day 12 & 13 setup

---
+++ src/main/java/org/example/Solution/day12/Day12Solver.java
@@ -0,0 +1,16 @@
+package org.example.Solution.day12;
+
+import org.example.Solution.AbstractDayXXSolver;
+
+public class Day12Solver extends AbstractDayXXSolver<Long> {
+
+    @Override
+    public Long partOneSolution() {
+        return null;
+    }
+
+    @Override
+    public Long partTwoSolution() {
+        return null;
+    }
+}
