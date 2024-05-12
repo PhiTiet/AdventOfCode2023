7c16b15ecf03c87b413db3e2e5212a3fcffb99ac
Phi Tiet <anhphi099@gmail.com>
2024-05-12 13:07:24 +0200

    implements sorting in grid

--- src/main/java/org/example/Solution/model/grid/GridElement.java
+++ src/main/java/org/example/Solution/model/grid/GridElement.java
@@ -7,8 +7,16 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 @Setter
-public class GridElement {
+public class GridElement implements Comparable<GridElement> {
     private long x;
     private long y;
     private String symbol;
+
+    @Override
+    public int compareTo(GridElement other) {
+        if (y == other.y){
+            return Long.compare(x, other.getX());
+        };
+        return Long.compare(y, other.y);
+    }
 }
--- src/test/java/org/example/Solution/utils/grid/GridTest.java
+++ src/test/java/org/example/Solution/utils/grid/GridTest.java
@@ -5,7 +5,10 @@ import org.example.Solution.model.grid.GridElement;
 import org.example.Solution.utils.FileReader;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class GridTest {
     private final FileReader fileReader = new FileReader();
@@ -13,7 +16,18 @@ class GridTest {
     @Test
     void grid() {
         List<String> gridValues = fileReader.getLines("/day10.txt");
-        var grid = new Grid<GridElement>(gridValues, GridElement.class);
+        var grid = new Grid<>(gridValues, GridElement.class);
         grid.print();
+    }
+
+    @Test
+    void sortsCorrectly(){
+        List<String> gridValues = fileReader.getLines("/day10.txt");
+        var grid = new Grid<>(gridValues, GridElement.class);
+        List<GridElement> elements = grid.getElements();
+        Collections.shuffle(elements);
+        Collections.sort(elements);
+        assertThat(elements.get(0).getX() == 0 && elements.get(0).getY() == 0);
+        assertThat(elements.get(1).getX() == 1 && elements.get(0).getY() == 0);
     }
 }
