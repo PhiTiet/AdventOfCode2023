641d024b60a3fb9339581b864a62686b33201d47
Phi Tiet <anhphi099@gmail.com>
2024-05-12 13:17:42 +0200

    setup for day16

---
+++ src/main/java/org/example/Solution/day16/Day16Solver.java
@@ -0,0 +1,16 @@
+package org.example.Solution.day16;
+
+import org.example.Solution.AbstractDayXXSolver;
+
+public class Day16Solver extends AbstractDayXXSolver<Long> {
+
+    @Override
+    public Long partOneSolution() {
+        return null;
+    }
+
+    @Override
+    public Long partTwoSolution() {
+        return null;
+    }
+}
