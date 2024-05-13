7509001ff8fb9a3da9786c3ffa3554566c2bcb8b
Phi Tiet <anhphi099@gmail.com>
2024-05-13 09:02:35 +0200

    Setup Lightbeam

--- src/main/java/org/example/Solution/day16/Day16Solver.java
+++ src/main/java/org/example/Solution/day16/Day16Solver.java
@@ -1,12 +1,21 @@
 package org.example.Solution.day16;
 
 import org.example.Solution.AbstractDayXXSolver;
+import org.example.Solution.day16.model.LightBeam;
 import org.example.Solution.day16.model.Mirror;
 import org.example.Solution.day16.model.MirrorGrid;
 import org.example.Solution.model.grid.Grid;
+import org.example.Solution.utils.model.Direction;
+
+import java.util.ArrayList;
+
+import static org.example.Solution.utils.ArrayListUtils.ArrayListOf;
 
 public class Day16Solver extends AbstractDayXXSolver<Long> {
     private final Grid<Mirror> grid = new MirrorGrid(getDefaultPuzzleInputLines());
+
+    private LightBeam initalLightbeam = new LightBeam(Direction.EAST, 0 ,0);
+    private ArrayList<LightBeam> lightbeams = ArrayListOf(initalLightbeam);
 
     @Override
     public Long partOneSolution() {
--- src/main/java/org/example/Solution/day16/model/LightBeam.java
+++ src/main/java/org/example/Solution/day16/model/LightBeam.java
@@ -1,11 +1,30 @@
 package org.example.Solution.day16.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.example.Solution.utils.model.Direction;
 
 @AllArgsConstructor
+@Getter
 public class LightBeam {
     private Direction direction;
     private long x;
     private long y;
+
+    public void travel(){
+        switch(direction){
+            case NORTH -> {
+                y--;
+            }
+            case EAST -> {
+                x++;
+            }
+            case SOUTH -> {
+                y++;
+            }
+            case WEST -> {
+                x--;
+            }
+        }
+    }
 }
