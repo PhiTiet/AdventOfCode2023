b636e28b9215bc9a55e8e58a3a6c9660b5cd6d84
Phi Tiet <anhphi099@gmail.com>
2024-05-13 08:55:21 +0200

    refactoring

--- src/main/java/org/example/Solution/day16/model/LightBeam.java
+++ src/main/java/org/example/Solution/day16/model/LightBeam.java
@@ -1,9 +1,11 @@
 package org.example.Solution.day16.model;
 
+import lombok.AllArgsConstructor;
 import org.example.Solution.utils.model.Direction;
 
+@AllArgsConstructor
 public class LightBeam {
-    private Direction direction = Direction.EAST;
+    private Direction direction;
     private long x;
     private long y;
 }
--- src/main/java/org/example/Solution/day16/model/MirrorType.java
+++ src/main/java/org/example/Solution/day16/model/MirrorType.java
@@ -1,9 +1,5 @@
 package org.example.Solution.day16.model;
 
 public enum MirrorType {
-    EMPTY,
-    DIAGONAL_LEFT_RIGHT,
-    DIAGONAL_RIGHT_LEFT,
-    VERTICAL,
-    HORIZONTAL
+    EMPTY, DIAGONAL_LEFT_RIGHT, DIAGONAL_RIGHT_LEFT, VERTICAL, HORIZONTAL
 }
