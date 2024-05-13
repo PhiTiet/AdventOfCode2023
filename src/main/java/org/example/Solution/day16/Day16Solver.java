17b4a9990fbc639435e0cec4133fd648b87bec0c
Phi Tiet <anhphi099@gmail.com>
2024-05-13 11:10:34 +0200

    Refactors partOne slightly and solves for part2

--- src/main/java/org/example/Solution/day16/Day16Solver.java
+++ src/main/java/org/example/Solution/day16/Day16Solver.java
@@ -13,23 +13,19 @@ import java.util.ArrayList;
 import static org.example.Solution.utils.ArrayListUtils.ArrayListOf;
 
 public class Day16Solver extends AbstractDayXXSolver<Long> {
-    private final MirrorGrid grid = new MirrorGrid(getDefaultPuzzleInputLines());
+    private MirrorGrid grid;
     private final LightBeamMirrorPropagator lightbeamMirrorPropogator = new LightBeamMirrorPropagator();
-
-    private LightBeam initalLightbeam = new LightBeam(Direction.SOUTH, new Position(0, 0));
-    private ArrayList<LightBeam> lightbeams = ArrayListOf(initalLightbeam);
     private ArrayList<LightBeam> newLightBeams = new ArrayList<>();
 
     @Override
     public Long partOneSolution() {
-        propagateLightBeamsAndEnergizeGrid();
-//        grid.printEnergized();
-        return grid.getElements().stream().filter(Mirror::isEnergized).count();
+        return getEnergizedCountAfterPropogatingLight(ArrayListOf(new LightBeam(Direction.SOUTH, new Position(0, 0))));
     }
 
-    private void propagateLightBeamsAndEnergizeGrid() {
+    private Long getEnergizedCountAfterPropogatingLight(ArrayList<LightBeam> lightBeams) {
+        grid = new MirrorGrid(getDefaultPuzzleInputLines());
         do {
-            for (var lightBeam : lightbeams) {
+            for (var lightBeam : lightBeams) {
                 var energizedBy = grid.getElementAt(lightBeam.getPosition()).getEnergizedBy();
 
                 if (energizedBy.contains(lightBeam.getDirection())){
@@ -48,13 +44,26 @@ public class Day16Solver extends AbstractDayXXSolver<Long> {
                 newLightBeams.addAll(nextLightBeams);
             }
             
-            lightbeams = newLightBeams;
+            lightBeams = newLightBeams;
             newLightBeams = new ArrayList<>();
-        } while (!lightbeams.isEmpty());
+        } while (!lightBeams.isEmpty());
+        return grid.getElements().stream().filter(Mirror::isEnergized).count();
     }
 
     @Override
     public Long partTwoSolution() {
-        return null;
+        var possibleInitialLightBeams = new ArrayList<LightBeam>();
+        int gridSize = grid.getGridSize();
+        for (int i = 0; i < gridSize; i++) {
+            possibleInitialLightBeams.add(new LightBeam(Direction.SOUTH, new Position(i,0)));
+            possibleInitialLightBeams.add(new LightBeam(Direction.NORTH, new Position(i, gridSize -1)));
+            possibleInitialLightBeams.add(new LightBeam(Direction.WEST, new Position(gridSize -1, i)));
+            possibleInitialLightBeams.add(new LightBeam(Direction.EAST, new Position(0, i)));
+        }
+        return possibleInitialLightBeams.stream()
+                .map(b -> getEnergizedCountAfterPropogatingLight(ArrayListOf(b)))
+                .max(Long::compareTo)
+                .orElseThrow();
     }
+
 }
