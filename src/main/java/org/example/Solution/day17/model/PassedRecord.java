b980a787c80f0ef7e8c087780966e70a1f94d6dc
Phi <anhphi099@gmail.com>
2024-05-14 23:16:19 +0200

    formatting

--- src/main/java/org/example/Solution/day17/model/PassedRecord.java
+++ src/main/java/org/example/Solution/day17/model/PassedRecord.java
@@ -2,6 +2,7 @@ package org.example.Solution.day17.model;
 
 import lombok.Builder;
 import org.example.Solution.utils.model.Direction;
+
 @Builder
 public record PassedRecord(long stepsTaken, long totalHeat, Direction direction) {
 }
