6e3cc4158623e92ee0f32d50f14eedb20132c4b8
Phi Tiet <anhphi099@gmail.com>
2024-05-16 10:37:25 +0200

    Optimized PassedRecords to use a map instead of a list

--- src/main/java/org/example/Solution/day17/model/PassedRecord.java
+++
@@ -1,7 +0,0 @@
-package org.example.Solution.day17.model;
-
-import lombok.Builder;
-
-@Builder
-public record PassedRecord(long stepsTaken, long totalHeat) {
-}
--- src/main/java/org/example/Solution/day17/model/PassedRecords.java
+++ src/main/java/org/example/Solution/day17/model/PassedRecords.java
@@ -2,36 +2,33 @@ package org.example.Solution.day17.model;
 
 import org.example.Solution.utils.model.Direction;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PassedRecords {
-    private Map<Direction, Boolean> hasPassedBy = new HashMap<>();
-    private Map<Direction, ArrayList<PassedRecord>> passedByRecords = new HashMap<>();
+    private Map<Direction, Map<Long, Long>> passedByRecords = new HashMap<>();
 
     public PassedRecords() {
         var values = List.of(Direction.values());
-        values.forEach(d -> hasPassedBy.put(d, false));
-        values.forEach(d -> passedByRecords.put(d, new ArrayList<>()));
+        values.forEach(d -> passedByRecords.put(d, new HashMap<>()));
     }
 
     public boolean shouldSkip(Path path){
-        if (!hasPassedBy.get(path.getDirection())){
+        if (passedByRecords.get(path.getDirection()).isEmpty()){
             return false;
         }
         var records = passedByRecords.get(path.getDirection());
-        return records.stream().anyMatch(a -> a.totalHeat() < path.getTotalHeat() && a.stepsTaken() <= path.getStepsTaken());
+        return records.entrySet().stream().anyMatch(a -> a.getKey() <= path.getStepsTaken() && a.getValue() < path.getTotalHeat());
     }
 
     public void addRecord(Path path) {
-        hasPassedBy.replace(path.getDirection(), true);
-        passedByRecords.get(path.getDirection()).add(
-                PassedRecord.builder()
-                        .totalHeat(path.getTotalHeat())
-                        .stepsTaken(path.getStepsTaken())
-                .build());
-
+        var records = passedByRecords.get(path.getDirection());
+        if (!records.containsKey(path.getStepsTaken())){
+            records.put(path.getStepsTaken(), path.getTotalHeat());
+        }
+        if (records.get(path.getStepsTaken()) > path.getTotalHeat()){
+            records.replace(path.getStepsTaken(), path.getTotalHeat());
+        }
     }
 }
