5b4c6b9cda37d9cebb67d6c4fe49f2abc08ac7a3
Phi Tiet <anhphi099@gmail.com>
2024-05-16 10:25:00 +0200

    adds a depth first passthrough to set a few lowerbounds for paths

--- src/main/java/org/example/Solution/day17/Day17Solver.java
+++ src/main/java/org/example/Solution/day17/Day17Solver.java
@@ -6,9 +6,11 @@ import org.example.Solution.day17.model.HeatTile;
 import org.example.Solution.day17.model.Path;
 import org.example.Solution.model.grid.Position;
 import org.example.Solution.utils.ArrayListUtils;
+import org.example.Solution.utils.model.Direction;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
@@ -24,17 +26,20 @@ public class Day17Solver extends AbstractDayXXSolver<Long> {
     }
 
     private Long findShortedPath(ArrayList<Path> paths) {
+        optimizeGridWithDepthFirstSearches();
         var newPaths = new ArrayList<Path>();
         var results = new ArrayList<Path>();
         while (!paths.isEmpty()) {
+            System.out.println(paths.size());
+            System.out.println(paths.get(0).getTotalHeat());
             for (var path : paths) {
 
                 if ((path.getTotalHeat() > 1012)) {
                     continue;
                 }
-//                if (!results.isEmpty() && results.stream().anyMatch(a -> a.getTotalHeat() < path.getTotalHeat())) {
-//                    continue;
-//                }
+                if (!results.isEmpty() && results.stream().anyMatch(a -> a.getTotalHeat() < path.getTotalHeat())) {
+                    continue;
+                }
 
                 HeatTile currentTile = grid.getElementAt(path.getPosition());
                 if (path.getPosition().equals(targetPosition)) {
@@ -44,7 +49,7 @@ public class Day17Solver extends AbstractDayXXSolver<Long> {
                     continue;
                 }
 
-                if (currentTile.getPassedRecords().shouldSkip(path)){
+                if (currentTile.getPassedRecords().shouldSkip(path)) {
                     continue;
                 }
                 Path left = new Path(path).withStepsTaken(0L).withDirection(getTurnLeft(path.getDirection()));
@@ -59,7 +64,7 @@ public class Day17Solver extends AbstractDayXXSolver<Long> {
             newPaths = new ArrayList<>();
         }
         var sortedHeatsAndPaths = new TreeMap<>(results.stream().collect(Collectors.groupingBy(Path::getTotalHeat)));
-        List<ArrayList<Position>> fastestRoutes = sortedHeatsAndPaths.firstEntry().getValue().stream().map( Path::getPrevious).toList();
+        List<ArrayList<Position>> fastestRoutes = sortedHeatsAndPaths.firstEntry().getValue().stream().map(Path::getPrevious).toList();
 
         for (var fastestRoute : fastestRoutes) {
             grid.printElements(fastestRoute, "#");
@@ -67,6 +72,48 @@ public class Day17Solver extends AbstractDayXXSolver<Long> {
         return results.stream()
                 .map(Path::getTotalHeat)
                 .min(Long::compareTo).orElseThrow() - grid.getElementAt(0L, 0L).getHeat();
+    }
+
+    private void optimizeGridWithDepthFirstSearches() {
+        var results = new ArrayList<Path>();
+        var pathToGoal = generatePaths();
+        for (int i = 0; i < 1000 ; i++) {
+            var path = new Path(EAST, new Position());
+            for (Direction direction : pathToGoal) {
+                path = path.withDirection(direction).withStepsTaken(0L);
+                var currentTile = grid.getElementAt(path.getPosition());
+                currentTile.getPassedRecords().addRecord(path);
+                path.addHeat(currentTile.getHeat());
+                if (path.willBeOutOfRange(grid.getGridSize())) {
+                    continue;
+                }
+                path.travel();
+            }
+            results.add(path);
+        }
+
+//        var paths = results.stream().map(Path::getPrevious).toList();
+//        grid.printElements(paths.getFirst(), "#");
+        System.out.println("Grid initialized with 1000 depth first paths");
+    }
+
+    private ArrayList<Direction> generatePaths() {
+        var pathToGoal = new ArrayList<Direction>();
+        int south = grid.getGridSize();
+        int east = grid.getGridSize();
+        while (south > 0 || east > 0) {
+            var actualSouth = Math.min(south, new Random().nextInt(3) + 1);
+            for (int j = 0; j < actualSouth; j++) {
+                pathToGoal.add(SOUTH);
+            }
+            south -= actualSouth;
+            var actualEast = Math.min(east, new Random().nextInt(3) + 1);
+            for (int j = 0; j < actualEast; j++) {
+                pathToGoal.add(EAST);
+            }
+            east -= actualEast;
+        }
+        return pathToGoal;
     }
 
     private void travel(Path path, HeatTile currentTile, ArrayList<Path> newPaths) {
